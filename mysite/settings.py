9c9fc1a66b84c638b9dac177017c9256ba6e729a
Eduardo Vicentini <edudiasvicentini@hotmail.com>
2020-06-04 14:43:38 -0300

    Removi informações sensiveis

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,10 +24,10 @@ TEMPLATES_DIR_COVID19 = os.path.join(TEMPLATES_DIR, 'covid19')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=435zdv*m9qci6)9uw)b9v7q)2w#qr+s5@_o5t4m9bz4mlohdq'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool
 
 ALLOWED_HOSTS = []
 
@@ -83,7 +84,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': config("DB_NAME"),
     }
 }
 
